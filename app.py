612b6965f67da63ffc9e85706e24ca7ff45b7411
AjayTomar10 <tomarajay262@gmail.com>
2024-11-15 02:55:32 +0530

    git-commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from pandas import json_normalize
 import folium
+from geopy.distance import geodesic
 
 app = Flask(__name__)
 
@@ -34,28 +35,38 @@ def get_clusters(lat, lng, kclusters=3):
 def index():
     return render_template('index.html')
 
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+
 @app.route('/api/map', methods=['POST'])
 def get_map():
     data = request.get_json()
     lat = data['latitude']
     lng = data['longitude']
-    
+    college_location = (lat, lng)
     df_clusters = get_clusters(lat, lng)
     if df_clusters is None:
         return jsonify({'error': 'No data found'}), 404
     
     # Generate map with clusters
-    map_bang = folium.Map(location=[lat, lng], zoom_start=12)
+    map_bang = folium.Map(location=[lat, lng], zoom_start=10)
     for _, row in df_clusters.iterrows():
+        apartment_location = (row['location.lat'], row['location.lng'])
+        distance = geodesic(college_location, apartment_location).kilometers  # Calculate distance
+
+        popup_content = f"<b>{row['name']}</b><br>Distance: {distance:.2f} km"
+        popup = folium.Popup(popup_content, max_width=200, min_width=150)  # Set width
         folium.CircleMarker(
             [row['location.lat'], row['location.lng']],
             radius=5,
             color='blue',
             fill=True,
             fill_opacity=0.6,
-            popup=row['name']
+            popup=popup
         ).add_to(map_bang)
-
+    folium.Marker([lat, lng], popup="My Location", icon=folium.Icon(color="red")).add_to(map_bang)
     map_bang.save('templates/map.html')
     return jsonify({'message': 'Map generated successfully'})
     
